1965416fb0ffc2fdab755518121b1bebfac0419e
sagar satarkar <sagarsatarkar999@gmail.com>
2023-04-20 15:11:04 +0530

    Revert "java files modified"
    This reverts commit 672eed80680cd0e842ba546643e3b709d855d78e.

--- src/main/java/LibraryBooks/Lib/Books/App.java
+++ src/main/java/LibraryBooks/Lib/Books/App.java
@@ -8,8 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        int x=10;
-int y=20;
         System.out.println( "Hello World!" );
     }
 }
--- src/test/java/LibraryBooks/Lib/Books/AppTest.java
+++ src/test/java/LibraryBooks/Lib/Books/AppTest.java
@@ -9,8 +9,6 @@ import junit.framework.TestSuite;
  */
 public class AppTest 
     extends TestCase
-int a=30;
-int b=40;
 {
     /**
      * Create the test case
