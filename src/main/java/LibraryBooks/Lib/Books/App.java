d29992d71f1197608b6a39b9f5555e72a1083b7e
sagar satarkar <sagarsatarkar999@gmail.com>
2023-04-20 13:54:37 +0530

    libraryBooks project added

---
+++ src/main/java/LibraryBooks/Lib/Books/App.java
@@ -0,0 +1,13 @@
+package LibraryBooks.Lib.Books;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
